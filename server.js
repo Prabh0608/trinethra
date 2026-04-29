7ff340e6751e7e0436392023fb91995e22c39f37
Prabhjot <sainiprabhjot75@gmail.com>
2026-04-29 23:59:08 +0530

    initialising my llama

---
+++ server.js
@@ -0,0 +1,11 @@
+const response = await fetch("http://localhost:11434/api/generate", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({
+    model: "llama3.2",
+    prompt: "say hello prabhjot",
+    stream: false,
+  }),
+});
+const data = await response.json();
+console.log(data.response);
